2c269e075adec2cfd2f89ce848b74c95d823ac42
justintsugranes <justintsugranes@proton.me>
2022-11-26 12:36:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Justin's WebLog
+
+An example blog app where I learned more about:
+
+- HTTP Requests
+  - URLs, endpoints,
+  - Methods,
+  - Headers
+- REST APIs
+  - Resources
+  - Parameters
+  - Queries
+
+Project uses the [JSONPLaceholder API](https://jsonplaceholder.typicode.com/) to render the first 10 example blog posts.
+
+Users can add a title and body text to the form and click "Post" to post new blogs to the form.
---
+++ index.js
@@ -0,0 +1,50 @@
+let postsArray = []
+const titleInput = document.getElementById('post-title')
+const bodyInput = document.getElementById('post-body')
+const form = document.getElementById('new-post')
+const blogList = document.getElementById('blog-list')
+
+function renderPosts() {
+  let html = ''
+  for (let post of postsArray) {
+    html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+  }
+  blogList.innerHTML = html
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((res) => res.json())
+  .then((data) => {
+    postsArray = data.slice(0, 10)
+    renderPosts()
+  })
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const postTitle = titleInput.value
+  const postBody = bodyInput.value
+  const data = {
+    title: postTitle,
+    body: postBody,
+  }
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  fetch('https://jsonplaceholder.typicode.com/posts', options)
+    .then((res) => res.json())
+    .then((post) => {
+      postsArray.unshift(post)
+      renderPosts()
+      form.reset()
+    })
+})
